74cbed231c6ce6d9d0505650417dea508d382207
松本義高 <yma2@matsumotoyoshitakanoMacBook-Pro.local>
2020-12-21 18:27:21 +0900

    add azure_fucntion for linux

---
+++ azure_functions/TimerTriggerCSharp1.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace Company.Function
+{
+    public static class TimerTriggerCSharp1
+    {
+        [FunctionName("TimerTriggerCSharp1")]
+        public static void Run([TimerTrigger("*/10 * * * * *")]TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+    }
+}
