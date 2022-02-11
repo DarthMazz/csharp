8ae390e46d8d20895e912fb5ea55e342fe733604
YOSHITAKA Matsumoto <yo4taka.ma2moto@gmail.com>
2022-02-11 20:15:14 +0900

    add json

---
+++ json/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.Json;
+using System.Collections.Generic;
+
+namespace json
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var jsonString = System.IO.File.ReadAllText("./mydata.json", System.Text.Encoding.UTF8);
+            var json = JsonSerializer.Deserialize<Dictionary<string, object>>(jsonString);
+            foreach (var item in json.Keys)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine(json[item]);
+                var results = json[item] as Array;
+                Console.WriteLine(results.GetValue(0));
+            }
+
+            // var results = json["titles"] as List<object>;
+            // Console.WriteLine(json);
+        }
+    }
+}
