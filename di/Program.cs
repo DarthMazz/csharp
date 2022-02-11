d5e7ca6d64d88190648124368aa33b4ea26a7494
YOSHITAKA Matsumoto <yo4taka.ma2moto@gmail.com>
2022-02-11 20:53:25 +0900

    add di

---
+++ di/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace di
+{
+    interface IMyService
+    {
+        
+    }
+
+    class MyService : IMyService
+    {
+        public MyService()
+        {
+            Console.WriteLine($"MyService Constructor");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            // serviceCollection.AddTransient<IMyService, MyService>();
+            serviceCollection.AddSingleton<IMyService, MyService>();
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+            var service1 = serviceProvider.GetService<IMyService>();
+            Console.WriteLine($"service1 HashCode : {service1.GetHashCode()}");
+            var service2 = serviceProvider.GetService<IMyService>();
+            Console.WriteLine($"service2 HashCode : {service2.GetHashCode()}");
+        }
+    }
+}
