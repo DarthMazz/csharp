a1019cffdce98c51d50433e529bf3d72d3420057
YOSHITAKA matsumot <yo4taka.ma2moto@gmail.com>
2023-09-13 21:41:23 +0900

    JsonNodeを使う

--- json/Program.cs
+++ json/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Text.Json;
 using System.Collections.Generic;
+using System.Text.Json.Nodes;
 
 namespace json
 {
@@ -14,12 +15,28 @@ namespace json
             {
                 Console.WriteLine(item);
                 Console.WriteLine(json[item]);
-                var results = json[item] as Array;
-                Console.WriteLine(results.GetValue(0));
+                // var results = json[item] as ;
+                // Console.WriteLine(results.GetValue(0));
             }
 
             // var results = json["titles"] as List<object>;
-            // Console.WriteLine(json);
+            Console.WriteLine(json);
+            var jnode = JsonSerializer.Deserialize<JsonNode>(jsonString);
+            Console.WriteLine(jnode);
+            Console.WriteLine(jnode["titles"]);
+            var item0 = jnode["titles"][0][0][0];
+            Console.WriteLine(item0);
+            var list = jnode["titles"][0][0].AsArray();
+            foreach (var jitem in list)
+            {
+                Console.WriteLine(jitem);
+            }
+
+            jnode["test"] = "test";
+            Console.WriteLine(jnode.ToString());
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            Console.WriteLine(jnode!.ToJsonString(options));
+            // output:
         }
     }
 }
